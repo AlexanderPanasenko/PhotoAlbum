e0d02a62943c40017b371e20f0575676319b88cf
Aleksandr Panasenko <104031583+AlexanderPanasenko@users.noreply.github.com>
2022-06-18 13:37:50 +0300

    addModel

--- HW14_PhotoAlbum/Model/Model.swift
+++ HW14_PhotoAlbum/Model/Model.swift
@@ -5,4 +5,28 @@
 //  Created by Alexander Panasenko on 18.06.2022.
 //
 
-import Foundation
+import UIKit
+
+struct Albums: Hashable {
+var section: Sections
+var album: [Album]
+var name: String
+var icon: UIImage?
+}
+
+struct Album: Hashable {
+var image: UIImage?
+}
+
+enum Sections: String, CaseIterable {
+    case myAlbums = "My Albums"
+    case peopleAndPlacesAlbums = "People and places Albums"
+    case mediaTypes = "Media types"
+    case other = "Other"
+}
+
+struct OtherAlbums: Hashable {
+    var icon: UIImage?
+    var name: String
+}
+
