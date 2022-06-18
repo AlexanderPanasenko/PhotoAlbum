68b93362f1d466fff2d1f58b2cfb58d0c8c7f963
Aleksandr Panasenko <104031583+AlexanderPanasenko@users.noreply.github.com>
2022-06-18 13:01:59 +0300

    addFeature

---
+++ HW14_PhotoAlbum/Album/HeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  HeaderView.swift
+//  HW14_PhotoAlbum
+//
+//  Created by Alexander Panasenko on 18.06.2022.
+//
+
+import Foundation
---
+++ HW14_PhotoAlbum/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  HW14_PhotoAlbum
+//
+//  Created by Alexander Panasenko on 18.06.2022.
+//
+
+import Foundation
